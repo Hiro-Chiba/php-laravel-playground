b20bfa06868dbc05bc567e2d9901f06b485501af
Hiro <Hiro.work1011@gmail.com>
2025-12-15 03:19:26 +0900

    レイアウトファイルの作成

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>My App</title>
+</head>
+<body>
+
+  {{-- 各画面の content がここに差し込まれる --}}
+  @yield('content')
+
+</body>
+</html>
